7a56fe79620372d7126a7b49175eb4d61f9d0162
Krzysztof Kamyczek <krzysztof.kamyczek@dxc.com>
2021-06-09 09:15:37 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
         .binaryTarget(
             name: "libzmq_pw",
             url: "https://github.com/kkamyczek/Storage/raw/master/libzmq_pw.xcframework.zip",
-            checksum: "3974adf18045ea3e6b5cca158cdebcb3176c7dc36e324a4d316f3a449a0b0cfc"
+            checksum: "f04cc86c0b7cf11e59df5432af45f2eb598faf04c40d520aedcb5e31069d0518"
         ),
          .binaryTarget(
             name: "sodium",
             url: "https://github.com/kkamyczek/Storage/raw/master/sodium.xcframework.zip",
-            checksum: "3974adf18045ea3e6b5cca158cdebcb3176c7dc36e324a4d316f3a449a0b0cfc"
+            checksum: "9ecbca2dc5197bc95ae7caeedbc2257b6b7970e2b1b725f0678260d85b7f3f21"
         )
     ])
