91b0323a43e36eff8a5ed6426cd86b0b3e7702f3
Krzysztof Kamyczek <krzysztof.kamyczek@dxc.com>
2021-06-09 08:21:41 +0200

    Added config

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "ssi_agent",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "ssi_agent", 
+            targets: ["ssi_agent"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ssi_agent",
+            url: "https://github.com/kkamyczek/Storage/blob/master/ssi_agent.xcframework.zip",
+            checksum: "b9d87d725565adf101d9bfa818039610"
+        )
+    ])
